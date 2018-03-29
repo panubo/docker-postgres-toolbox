eba82d062b3241dc3668a82c036d22799077d688
Andrew Cutler <andrew@panubo.com>
2018-03-29 11:13:24 +1100

    Fix tests

--- tests/test.sh
+++ tests/test.sh
@@ -37,8 +37,9 @@ cleanup postgres ${TEST_NAME}-create ${TEST_NAME}-dump
 echo "=> Test restore command"
 docker run -d --name postgres -p 5432:5432 -e POSTGRES_PASSWORD=password postgres:latest > /dev/null
 sleep 5
-docker run -t -i --name ${TEST_NAME}-create --link postgres $TEST_CONTAINER create-user-db foo
+docker run -t -i --name ${TEST_NAME}-create1 --link postgres $TEST_CONTAINER create-user-db foo
 docker run -t -i --name ${TEST_NAME}-dump --link postgres -e DUMP_DIR="/srv" -v /srv:/srv $TEST_CONTAINER dump
 docker run -t -i --name ${TEST_NAME}-delete --link postgres $TEST_CONTAINER delete-user-db foo
+docker run -t -i --name ${TEST_NAME}-create2 --link postgres $TEST_CONTAINER create-user-db foo
 docker run -t -i --name ${TEST_NAME}-restore --link postgres -e DUMP_DIR="/srv" -v /srv:/srv $TEST_CONTAINER restore foo
-cleanup postgres ${TEST_NAME}-create ${TEST_NAME}-dump ${TEST_NAME}-delete ${TEST_NAME}-restore
+cleanup postgres ${TEST_NAME}-create1 ${TEST_NAME}-dump ${TEST_NAME}-delete ${TEST_NAME}-restore ${TEST_NAME}-create2
