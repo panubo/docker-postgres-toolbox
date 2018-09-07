3192805aa9765758f1fbef6b4a85c0966429271c
Andrew Cutler <andrew@panubo.com>
2018-09-07 18:18:34 +1000

    Add ability to connect to named db

--- tests/test.sh
+++ tests/test.sh
@@ -8,33 +8,33 @@ CWD="$(dirname $0)/"
 
 . ${CWD}functions.sh
 
-echo "=> Test vacuum command"
+echo "===> Test vacuum command"
 docker run -d --name postgres -p 5432:5432 -e POSTGRES_PASSWORD=password postgres:latest > /dev/null
 sleep 5
 docker run -t -i --name $TEST_NAME --link postgres $TEST_CONTAINER vacuum
 cleanup postgres $TEST_NAME
 
-echo "=> Test create-user-db command"
+echo "===> Test create-user-db command"
 docker run -d --name postgres -p 5432:5432 -e POSTGRES_PASSWORD=password postgres:latest > /dev/null
 sleep 5
 docker run -t -i --name $TEST_NAME --link postgres $TEST_CONTAINER create-user-db foo foopass
 cleanup postgres $TEST_NAME
 
-echo "=> Test delete-user-db command"
+echo "===> Test delete-user-db command"
 docker run -d --name postgres -p 5432:5432 -e POSTGRES_PASSWORD=password postgres:latest > /dev/null
 sleep 5
 docker run -t -i --name ${TEST_NAME}-create --link postgres $TEST_CONTAINER create-user-db foo
 docker run -t -i --name ${TEST_NAME}-delete --link postgres $TEST_CONTAINER delete-user-db foo
 cleanup postgres ${TEST_NAME}-create ${TEST_NAME}-delete
 
-echo "=> Test save command"
+echo "===> Test save command"
 docker run -d --name postgres -p 5432:5432 -e POSTGRES_PASSWORD=password postgres:latest > /dev/null
 sleep 5
 docker run -t -i --name ${TEST_NAME}-create --link postgres $TEST_CONTAINER create-user-db foo
 docker run -t -i --name ${TEST_NAME}-save --link postgres -e DUMP_DIR="/srv" $TEST_CONTAINER save
 cleanup postgres ${TEST_NAME}-create ${TEST_NAME}-save
 
-echo "=> Test load command"
+echo "===> Test load command"
 docker run -d --name postgres -p 5432:5432 -e POSTGRES_PASSWORD=password postgres:latest > /dev/null
 sleep 5
 docker run -t -i --name ${TEST_NAME}-create1 --link postgres $TEST_CONTAINER create-user-db foo
