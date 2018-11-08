200e9b8183d91250e1acfad8d954fd6da344306d
Andrew Cutler <andrew@panubo.com>
2018-11-08 15:15:00 +1100

    Add create-readonly-user command

--- tests/test.sh
+++ tests/test.sh
@@ -8,34 +8,43 @@ CWD="$(dirname $0)/"
 
 . ${CWD}functions.sh
 
+POSTGRES_VERSION=10.5
+
 echo "===> Test vacuum command"
-docker run -d --name postgres -p 5432:5432 -e POSTGRES_PASSWORD=password postgres:latest > /dev/null
+docker run -d --name postgres -p 5432:5432 -e POSTGRES_PASSWORD=password postgres:$POSTGRES_VERSION > /dev/null
 sleep 5
 docker run -t -i --name $TEST_NAME --link postgres $TEST_CONTAINER vacuum
 cleanup postgres $TEST_NAME
 
 echo "===> Test create-user-db command"
-docker run -d --name postgres -p 5432:5432 -e POSTGRES_PASSWORD=password postgres:latest > /dev/null
+docker run -d --name postgres -p 5432:5432 -e POSTGRES_PASSWORD=password postgres:$POSTGRES_VERSION > /dev/null
 sleep 5
 docker run -t -i --name $TEST_NAME --link postgres $TEST_CONTAINER create-user-db foo foopass
 cleanup postgres $TEST_NAME
 
 echo "===> Test delete-user-db command"
-docker run -d --name postgres -p 5432:5432 -e POSTGRES_PASSWORD=password postgres:latest > /dev/null
+docker run -d --name postgres -p 5432:5432 -e POSTGRES_PASSWORD=password postgres:$POSTGRES_VERSION > /dev/null
 sleep 5
 docker run -t -i --name ${TEST_NAME}-create --link postgres $TEST_CONTAINER create-user-db foo
 docker run -t -i --name ${TEST_NAME}-delete --link postgres $TEST_CONTAINER delete-user-db foo
 cleanup postgres ${TEST_NAME}-create ${TEST_NAME}-delete
 
+echo "===> Test create-readonly-user command"
+docker run -d --name postgres -p 5432:5432 -e POSTGRES_PASSWORD=password postgres:$POSTGRES_VERSION > /dev/null
+sleep 5
+docker run -t -i --name ${TEST_NAME}a --link postgres $TEST_CONTAINER create-user-db foo foopass_rw
+docker run -t -i --name ${TEST_NAME}b --link postgres $TEST_CONTAINER create-readonly-user foo foo_ro foopass_ro
+cleanup postgres ${TEST_NAME}a ${TEST_NAME}b
+
 echo "===> Test save command"
-docker run -d --name postgres -p 5432:5432 -e POSTGRES_PASSWORD=password postgres:latest > /dev/null
+docker run -d --name postgres -p 5432:5432 -e POSTGRES_PASSWORD=password postgres:$POSTGRES_VERSION > /dev/null
 sleep 5
 docker run -t -i --name ${TEST_NAME}-create --link postgres $TEST_CONTAINER create-user-db foo
 docker run -t -i --name ${TEST_NAME}-save --link postgres -e DUMP_DIR="/srv" $TEST_CONTAINER save
 cleanup postgres ${TEST_NAME}-create ${TEST_NAME}-save
 
 echo "===> Test load command"
-docker run -d --name postgres -p 5432:5432 -e POSTGRES_PASSWORD=password postgres:latest > /dev/null
+docker run -d --name postgres -p 5432:5432 -e POSTGRES_PASSWORD=password postgres:$POSTGRES_VERSION > /dev/null
 sleep 5
 docker run -t -i --name ${TEST_NAME}-create1 --link postgres $TEST_CONTAINER create-user-db foo
 docker run -t -i --name ${TEST_NAME}-save --link postgres -e DUMP_DIR="/srv" -v /srv:/srv $TEST_CONTAINER save
